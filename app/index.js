24b7d406531337cf1aab3e949fa1549f53bef4b5
Revath S Kumar <rsk@revathskumar.com>
2013-03-12 14:46:59 +0530

    adding Gruntfile and basic stylesheet

--- app/index.js
+++ app/index.js
@@ -58,3 +58,15 @@ MariaGenerator.prototype.indexFile = function indexFile() {
     this.template('app/index.html');
   }
 };
+
+MariaGenerator.prototype.gruntfile = function gruntfile() {
+  if (this.testFramework === 'jasmine') {
+    this.write('Gruntfile.js', this.read('Gruntfile.js').replace(/mocha/g, 'jasmine'));
+  } else {
+    this.template('Gruntfile.js');
+  }
+};
+
+MariaGenerator.prototype.mainStylesheet = function mainStylesheet() {
+  this.write('app/styles/main.css', 'body {\n    background: #fafafa;\n}\n\n.hero-unit {\n    margin: 50px auto 0 auto;\n    width: 300px;\n}');
+};
